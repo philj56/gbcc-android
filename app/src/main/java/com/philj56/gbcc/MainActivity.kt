5eb6bae61144877000e162606a245b509e2c57ae
Phil Jones <philj56@gmail.com>
2020-04-12 01:37:21 +0100

    Various changes to the main activity.
    - Add ability to create folders, delete roms, and import saves
    - Change from using an action bar to androidx Toolbar. This allows
      animation between the choosing a rom state, and deleting files state.
    - Add lots of animations to go with the new features.

--- app/src/main/java/com/philj56/gbcc/MainActivity.kt
+++ app/src/main/java/com/philj56/gbcc/MainActivity.kt
@@ -10,28 +10,37 @@
 
 package com.philj56.gbcc
 
+import android.animation.*
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.app.Dialog
 import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
 import android.graphics.Color
-import android.media.AudioManager
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.net.Uri
+import android.os.AsyncTask
 import android.os.Bundle
 import android.provider.OpenableColumns
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.*
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.addListener
+import androidx.core.content.ContextCompat
+import androidx.core.view.forEach
+import androidx.core.view.forEachIndexed
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.DialogFragment
 import androidx.preference.PreferenceManager
+import androidx.transition.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import java.io.FileOutputStream
+import kotlin.math.max
 
 
 private const val IMPORT_REQUEST_CODE: Int = 10
@@ -40,41 +49,140 @@ private const val BACK_DELAY: Int = 2000
 class MainActivity : AppCompatActivity() {
 
     private lateinit var files: ArrayList<File>
-    private lateinit var adapter: ArrayAdapter<File>
+    private lateinit var adapter: FileAdapter
     private lateinit var currentDir: File
     private lateinit var baseDir: File
 
     private var nightMode: Boolean = false
     private var timeBackPressed: Long = 0
 
+    private val toolbarTransition = CircularReveal()
+        .setDuration(300)
+        .setInterpolator(AccelerateDecelerateInterpolator())
+    private val fileTransition = Slide(Gravity.END).setDuration(300)
+    private val showTransition = ChangeBounds()
+    private val deleteTransitionSet = TransitionSet()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         nightMode = applicationContext.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
         if (nightMode) {
-            setTheme(R.style.AppThemeDark)
+            setTheme(R.style.AppThemeDark_NoActionBar)
         } else {
-            setTheme(R.style.AppTheme)
+            setTheme(R.style.AppTheme_NoActionBar)
         }
         super.onCreate(savedInstanceState)
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false)
         baseDir = getExternalFilesDir(null) ?: filesDir
         currentDir = baseDir
         setContentView(R.layout.activity_main)
+
+        // Setup the toolbars
+        mainToolbar.setTitle(R.string.app_name)
+        mainToolbar.inflateMenu(R.menu.main_menu)
+        mainToolbar.setOnMenuItemClickListener { item -> onMenuItemClick(item) }
+        fileToolbar.setTitle(R.string.create_folder)
+        fileToolbar.inflateMenu(R.menu.file_menu)
+        fileToolbar.setOnMenuItemClickListener { item -> onMenuItemClick(item) }
+        fileToolbar.visibility = View.GONE
+
+        toolbarTransition.addTarget(mainToolbar)
+        toolbarTransition.addTarget(fileToolbar)
+
+        val scene = Scene(fileList)
+        scene.setEnterAction {
+            fileList.forEach { view ->
+                view.visibility = View.VISIBLE
+            }
+            updateFiles()
+        }
+
+        deleteTransitionSet.addListener(object: TransitionListenerAdapter() {
+            override fun onTransitionEnd(transition: Transition) {
+                Log.d("Selected", adapter.selected.toString())
+                adapter.selected.forEach { position ->
+                    val file = adapter.getItem(position)
+                    files.remove(file)
+                    file?.deleteRecursively()
+                }
+                clearSelection()
+                fileList.forEach { view ->
+                    showTransition.addTarget(view)
+                }
+                TransitionManager.go(scene, showTransition)
+            }
+        })
+
+        deleteTransitionSet.addTransition(fileTransition)
+        deleteTransitionSet.addTransition(toolbarTransition)
+        deleteTransitionSet.ordering = TransitionSet.ORDERING_TOGETHER
+
         adapter = FileAdapter(this, R.layout.file_entry, R.id.fileEntry, files)
         fileList.adapter = adapter
         fileList.setOnItemClickListener{ _, _, position, _ ->
-            val file = fileList.adapter.getItem(position) as File
-            if (file.isDirectory) {
-                currentDir = file
-                updateFiles()
+            if (adapter.selected.isEmpty()) {
+                val file = fileList.adapter.getItem(position) as File
+                if (file.isDirectory) {
+                    currentDir = file
+                    updateFiles()
+                } else {
+                    switchToGL(file.toString())
+                }
             } else {
-                switchToGL(file.toString())
+                val item = fileList.getChildAt(position - fileList.firstVisiblePosition)
+                if (position in adapter.selected) {
+                    adapter.selected.remove(position)
+                    item.background.clearColorFilter()
+                    if (adapter.selected.isEmpty()) {
+                        TransitionManager.beginDelayedTransition(mainLayout, toolbarTransition)
+                        clearSelection()
+                    }
+                } else {
+                    adapter.selected.add(position)
+                    item.background.colorFilter = PorterDuffColorFilter(
+                        ContextCompat.getColor(this, R.color.fileSelected),
+                        PorterDuff.Mode.SRC)
+                }
+                item.invalidateDrawable(item.background)
             }
         }
+        fileList.setOnItemLongClickListener{ _, _, position, _ ->
+            if (adapter.selected.isEmpty()) {
+                val item = fileList.getChildAt(position - fileList.firstVisiblePosition)
+                adapter.selected.add(position)
+                item.background.colorFilter = PorterDuffColorFilter(
+                    ContextCompat.getColor(this, R.color.fileSelected),
+                    PorterDuff.Mode.SRC)
+                item.invalidateDrawable(item.background)
+
+                TransitionManager.beginDelayedTransition(mainLayout, toolbarTransition)
+
+                fileToolbar.visibility = View.VISIBLE
+                mainToolbar.visibility = View.GONE
+            }
+            return@setOnItemLongClickListener true
+        }
+    }
+
+    override fun onPause() {
+        clearSelection()
+        super.onPause()
     }
 
     override fun onContentChanged() {
         super.onContentChanged()
         updateFiles()
+    }
+
+    private fun clearSelection() {
+        if (adapter.selected.isNotEmpty()) {
+            adapter.selected.clear()
+            fileList.forEach { view ->
+                view.background.clearColorFilter()
+                view.invalidateDrawable(view.background)
+            }
+        }
+        mainToolbar.visibility = View.VISIBLE
+        fileToolbar.visibility = View.GONE
     }
 
     private fun updateFiles() {
@@ -96,18 +204,35 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
+    private fun onMenuItemClick(item: MenuItem): Boolean {
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.importItem -> performFileSearch()
             R.id.settingsItem -> startActivity(Intent(this, SettingsActivity::class.java))
+            R.id.folderItem -> {
+                val dialog = CreateFolderDialogFragment()
+                dialog.show(supportFragmentManager, "")
+            }
+            R.id.cancelItem -> clearSelection()
+            R.id.deleteItem -> {
+                fileList.forEachIndexed { index, view ->
+                    if (fileList.firstVisiblePosition + index in adapter.selected) {
+                        fileTransition.addTarget(view)
+                    }
+                }
+                val scene = Scene(fileList)
+                scene.setEnterAction {
+                    fileList.forEachIndexed { index, view ->
+                        if (fileList.firstVisiblePosition + index in adapter.selected) {
+                            view.visibility = View.GONE
+                        }
+                    }
+                }
+                TransitionManager.go(scene, deleteTransitionSet)
+                //TransitionManager.beginDelayedTransition(mainLayout, deleteTransitionSet)
+            }
         }
-
-        return super.onOptionsItemSelected(item)
+        return true
     }
 
     private fun switchToGL(filename: String) {
@@ -117,14 +242,24 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    fun createFolder(name: String) {
+        if (currentDir.resolve(name).mkdirs()) {
+            updateFiles()
+        } else {
+            Toast.makeText(
+                baseContext,
+                getString(R.string.message_failed_create_folder).format(name),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
     private fun performFileSearch() {
         val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
             addCategory(Intent.CATEGORY_OPENABLE)
-            type = "*/*"
+            type = "application/octet-stream"
         }
-
         intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
-
         startActivityForResult(intent, IMPORT_REQUEST_CODE)
     }
 
@@ -146,17 +281,36 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun importFile(uri: Uri) {
+        Log.d("File", uri.toString())
         val iStream = contentResolver.openInputStream(uri)
         if (iStream == null) {
-            Toast.makeText(baseContext, getString(R.string.message_failed_import).format(uri), Toast.LENGTH_SHORT).show()
+            runOnUiThread {
+                Toast.makeText(
+                    baseContext,
+                    getString(R.string.message_failed_import).format(uri),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         } else {
             val name = getFileName(uri)
             if (name == null) {
-                Toast.makeText(baseContext, getString(R.string.message_failed_name), Toast.LENGTH_SHORT).show()
+                runOnUiThread {
+                    Toast.makeText(
+                        baseContext,
+                        getString(R.string.message_failed_name),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
                 return
             }
-            if (!name.matches(Regex(".*\\.gbc?"))) {
-                Toast.makeText(baseContext, getString(R.string.message_failed_import).format(name), Toast.LENGTH_SHORT).show()
+            if (!name.matches(Regex(".*\\.(gbc?|sav|s[0-9])"))) {
+                runOnUiThread {
+                    Toast.makeText(
+                        baseContext,
+                        getString(R.string.message_failed_import).format(name),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
                 return
             }
             val data = iStream.use { it.readBytes() }
@@ -171,15 +325,29 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, resultData)
         if (requestCode == IMPORT_REQUEST_CODE && resultCode == RESULT_OK && resultData != null) {
             val clipData = resultData.clipData
-            if (clipData != null) {
-                for (i in 0 until clipData.itemCount) {
-                    importFile(clipData.getItemAt(i).uri)
+            // Run the import in the background to avoid blocking the UI
+            AsyncTask.execute {
+                if (clipData != null) {
+                    if (clipData.itemCount >= 10) {
+                        // Give the user some notification that an import is occurring
+                        runOnUiThread {
+                            Toast.makeText(
+                                baseContext,
+                                getString(R.string.message_importing).format(clipData.itemCount),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                    for (i in 0 until clipData.itemCount) {
+                        importFile(clipData.getItemAt(i).uri)
+                    }
+                } else {
+                    resultData.data?.also { importFile(it) }
                 }
-            } else {
-                resultData.data?.also { importFile(it) }
+                // updateFiles needs to be run on the main thread, however
+                runOnUiThread { updateFiles() }
             }
         }
-        updateFiles()
     }
 
     override fun onBackPressed() {
@@ -203,6 +371,7 @@ class MainActivity : AppCompatActivity() {
 class FileAdapter(context: Context, resource: Int, textViewResourceId: Int, objects: List<File>)
     : ArrayAdapter<File>(context, resource, textViewResourceId, objects) {
 
+    val selected = HashSet<Int>()
     private val nightMode = context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
@@ -210,6 +379,13 @@ class FileAdapter(context: Context, resource: Int, textViewResourceId: Int, obje
         val textView = view.findViewById<TextView>(R.id.fileEntry)
         val imageView = view.findViewById<ImageView>(R.id.fileIcon)
 
+        if (position in selected) {
+            view.background.colorFilter = PorterDuffColorFilter(
+                ContextCompat.getColor(context, R.color.fileSelected),
+                PorterDuff.Mode.SRC)
+        } else {
+            view.background.clearColorFilter()
+        }
         if (textView.text.endsWith(".gbc")) {
             imageView.setImageResource(R.drawable.ic_file_gbc)
             imageView.clearColorFilter()
@@ -221,17 +397,88 @@ class FileAdapter(context: Context, resource: Int, textViewResourceId: Int, obje
             if (nightMode) {
                 imageView.setColorFilter(
                     Color.argb(179, 255, 255, 255),
-                    android.graphics.PorterDuff.Mode.SRC_IN
+                    PorterDuff.Mode.SRC_IN
                 )
             } else {
                 imageView.setColorFilter(
                     Color.argb(138, 0, 0, 0),
-                    android.graphics.PorterDuff.Mode.SRC_IN
+                    PorterDuff.Mode.SRC_IN
                 )
             }
         }
 
         textView.text = File(textView.text.toString()).nameWithoutExtension
         return view
+    }
+}
+
+class CreateFolderDialogFragment : DialogFragment() {
+    @SuppressLint("InflateParams")
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val textView = it.layoutInflater.inflate(R.layout.create_folder, null, false)
+            val input = textView?.findViewById<EditText>(R.id.createFolderInput)
+
+            val builder = AlertDialog.Builder(it)
+            builder.setTitle(R.string.create_folder)
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    if (activity is MainActivity) {
+                        (activity as MainActivity).createFolder(input?.text.toString())
+                    }
+                }
+                .setNegativeButton(android.R.string.cancel) { _, _ -> }
+                .setView(textView)
+
+            val dialog = builder.create()
+            dialog.setOnShowListener {
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = false
+            }
+
+            input?.addTextChangedListener { editor ->
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = editor?.isNotBlank() ?: false
+            }
+            return dialog
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
+
+class CircularReveal : Visibility() {
+    override fun onAppear(
+        sceneRoot: ViewGroup?,
+        view: View?,
+        startValues: TransitionValues?,
+        endValues: TransitionValues?
+    ): Animator {
+        if (view == null) {
+            return super.onAppear(sceneRoot, view, startValues, endValues)
+        }
+        val animator = ViewAnimationUtils.createCircularReveal(
+            view,
+            view.width / 2,
+            view.height / 2,
+            0.0f,
+            max(view.width, view.height).toFloat())
+        view.alpha = 0.0f
+        animator.addListener(
+            onStart = { view.alpha = 1.0f }
+        )
+        return animator
+    }
+
+    override fun onDisappear(
+        sceneRoot: ViewGroup?,
+        view: View?,
+        startValues: TransitionValues?,
+        endValues: TransitionValues?
+    ): Animator {
+        if (view == null) {
+            return super.onDisappear(sceneRoot, view, startValues, endValues)
+        }
+        return ViewAnimationUtils.createCircularReveal(
+            view,
+            view.width / 2,
+            view.height / 2,
+            max(view.width, view.height).toFloat(),
+            0.0f)
     }
 }
